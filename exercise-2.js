59d67b4ba0f086f136fe11109d7d8f1bf719d3ed
Katilina88 <caterinalograsso@gmail.com>
2023-01-15 11:30:47 +0100

    add optional chaining es

---
+++ exercise-2.js
@@ -0,0 +1,7 @@
+const order = {};
+
+//if (order && order.customer && order.customer.address && !order.customer.address.city) {
+  console.log(order.custumer?.address?.city ?? 'City is required');
+
+
+
